b6dd1c0c1cd433e219e65e14da84c1974c0c1c08
script <script@proton.mail>
2025-08-19 19:05:46 +0300

    feat: random service

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+
+@app.get("/random-number")
+def get_random_text():
+    return {"result": True, "number": random.randint(1, 100)}
+
+
+@app.get("/random-text")
+def get_random_text():
+    return {"result": True, "text": random.randint(1, 100)}
+
+
+@app.get("/random-number-text")
+def get_random_text():
+    return {"result": True, "number": random.randint(1, 100)}
